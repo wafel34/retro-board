c7e792a2e04c33ee81b2ed34e1de7c06f0c3c648
wafel <wafel34@gmail.com>
2021-12-13 22:09:50 +0100

    Add PrimeNg

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule
+  ],
+  exports: [
+    ButtonModule
   ]
 })
 export class SharedModule {
