f1bb29b9429289e4fb0d1a9623ed4ca5a16728e4
wafel <wafel34@gmail.com>
2022-01-10 21:42:31 +0100

    Create auth service

---
+++ src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import UserCredential = firebase.auth.UserCredential;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(public auth: AngularFireAuth) {
+  }
+
+  login(): Promise<UserCredential> {
+    return this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout(): Promise<void> {
+    return this.auth.signOut();
+  }
+}
